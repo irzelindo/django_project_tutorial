6f0c74d27a16baecec3183214617f2ffd435f60d
irzelindo <irzelindo.salvador@gmail.com>
2021-01-07 17:35:10 +0200

    Refactor index View from function to generic View

--- polls/views.py
+++ polls/views.py
@@ -8,14 +8,13 @@ from django.views import generic
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    # print(latest_question_list)
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    return render(request, 'polls/index.html', context)
+class indexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+        
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
