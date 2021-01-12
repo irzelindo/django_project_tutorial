16df1525f4623fe66b8454505b5f9c60127f57ae
irzelindo <irzelindo.salvador@gmail.com>
2021-01-12 12:46:41 +0200

    Update id with pk on polls urls

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,11 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5
-    path('<int:question_id>/', views.detail, name='poll_detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='poll_detail'),
     # ex: /polls/5/results
-    path('<int:question_id>/results/', views.results, name='poll_results'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='poll_results'),
     # ex: /polls/5/votes
     path('<int:question_id>/vote/', views.vote, name='poll_votes')
 ]
